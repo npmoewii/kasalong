3021f2eccb4bd51e50e70bd2a00a8223bc7a222b
asddsada <baipo_p@hotmail.com>
2016-10-21 23:35:55 +0700

    css regis staff

--- regis_staff.php
+++ regis_staff.php
@@ -5,31 +5,37 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/main.css" type="text/css">
+  <link rel="stylesheet" href="css/regis_staff.css" type="text/css">
   <script src="js/jquery.min.js"></script>
 </head>
-<body>
+    <nav>
+        <div class="navbar">
+          <div class="heading">
+            <div class="title">KASALONG</div>
+          </div>
+    </nav>
+    <div class="navbar"></div>
     <form action="php/add_staff.php" method="POST">
-        อัพโหลดรูปโปรไฟล์ <input type="file" name="img_prof" required><br>
-        ชื่อ<input type="text" name="name"><br>
-        นามสกุล<input type="text" name="surname"><br>
-        ชื่อเล่น<input type="text" name="nickname"><br>
-        รหัสประจำตัวนิสิต <input type="text" name="idstu"><br>
-        คณะ
-        <select name="faculty">
+        <p id="head">staff register</p>
+        <!--<img alt="profile picture" src=""><br/> -->
+        <input type="file" name="img_prof" required id="pic">        
+        <input type="text" name="name" placeholder="ชื่อ">
+        <input type="text" name="surname" placeholder="นามสกุล" >
+        <input type="text" name="nickname" placeholder="ชื่อเล่น">        
+        <input type="text" name="idstu" placeholder="รหัสประจำตัวนิสิต">
+        <select name="faculty"> 
             <option value="Engineering">วิศวกรรมศาสตร์</option>
         </select>
-        ชั้นปี
         <select name="year">
             <option value="1">ปี 1 </option>
             <option value="2">ปี 2 </option>
             <option value="3">ปี 3 </option>
             <option value="4">ปี 4 </option>
-        </select><br>
-        Contact<br>
-        เบอร์โทรศัพท์มือถือ <input type="text" name="phone"><br>
-        Line <input type="text" name="lineid"><br>
-        Facebook <input type="text" name="facebook"><br>
-        <button type="submit" name="btn-submit">Submit</button>
+        </select>
+        <input type="text" name="phone" placeholder="เบอร์โทรศัพท์">
+        <input type="text" name="lineid" placeholder="Line">
+        <input type="text" name="facebook" placeholder="facebook"><br/>
+        <button type="submit" name="btn-submit">Submit</button>              
     </form>
 </body>
 </html>
