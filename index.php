fc789f09157e1f9e89703be50c8cec5b294a2ddf
ktpunnisa <ktpunnisa@gmail.com>
2016-10-21 20:32:33 +0700

    หน้าแรก

--- index.php
+++ index.php
@@ -11,27 +11,49 @@
 <body>
   <nav>
     <div class="navbar">
-      <div class="heading">
-        <div class="title">KASALONG</div>
-        <button type="button" id="navbutton">Menu</button>
-      </div>
       <ul class="nav">
-        <li><a href="#">รายละเอียดค่าย</a></li>
-        <li><a href="#">ตารางกิจกรรม</a></li>
-        <li><a href="#">ถาม-ตอบ</a></li>
-        <li><a href="#">ติดต่อสอบถาม</a></li>
+        <li><a href="#section1">KASALONG</a></li>
+        <li><a href="#section2">รายละเอียดค่าย</a></li>
+        <li><a href="#section3">กำหนดการ</a></li>
+        <li><a href="#section4">ถาม-ตอบ</a></li>
+        <li><a href="#section5">ติดต่อสอบถาม</a></li>
         <li class="right"><a href="login.php">Login</a></li>
       </ul>
     </div>
   </nav>
-  <div class="main" style="margin-top:50px;">
-    <div class="jumbo">
-      <div class="jumbotext">Kasalong Camp</div>
-      <hr>
+  
+  <section id="section1">
+    <div class="main" style="margin-top:50px;">
+      <div class="jumbo">
+        <div class="jumbotext">Kasalong Camp</div>
+      </div>
+      <div class="jumbopadding">
+        <a href="regis_stu.php">Register for Student</a>
+        <a href="regis_staff.php">Register for Staff</a>
+      </div>
     </div>
-    <a href="regis_stu.php">Register for Student</a>
-    <a href="regis_staff.php">Register for Staff</a>
-  </div>
   <script src="js/main.js"></script>
+  </section>
+  <section id="section2">
+    <div class="jumbo">
+      <div class="jumbohead">รายละเอียดค่าย</div>
+    </div>
+  </section>
+  <section id="section3">
+    <div class="jumbo">
+      <div class="jumbohead">กำหนดการ</div>
+    </div>
+  </section>
+  <section id="section4">
+    <div class="jumbo">
+      <div class="jumbohead">ถาม-ตอบ</div>
+    </div>
+  </section>
+  <section id="section5">
+    <div class="jumbo">
+      <div class="jumbohead">ติดต่อสอบถาม</div>
+    </div>
+  </section>
+  <footer>KASALONG CAMP</footer>
 </body>
 </html>
