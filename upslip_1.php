6182c8fe9a2993bb5ff2a599dc4e06732e8f66db
asddsada <baipo_p@hotmail.com>
2016-10-24 00:08:17 +0700

    upslip front page

--- upslip_1.php
+++ upslip_1.php
@@ -4,11 +4,13 @@
     <title>Prepare upload slip</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/main.css" type="text/css">
+  	<link rel="stylesheet" href="css/regis_staff.css" type="text/css">
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
     <form action="upslip.php" method="post">
-        กรอกเลขประจำตัวประชาชน<input type="text" name="nationid">
+    	<p id="head">กรอกเลขประจำตัวประชาชน</p>
+        <input type="text" name="nationid">
         <button type="submit" name="btn-submit">Submit</button>
     </form>
 </body>
