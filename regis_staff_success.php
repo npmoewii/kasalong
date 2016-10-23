03b37eebc0dc889e8d9b0a78981b0591a0248f90
asddsada <baipo_p@hotmail.com>
2016-10-23 23:36:09 +0700

    success page

--- regis_staff_success.php
+++ regis_staff_success.php
@@ -5,9 +5,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/main.css" type="text/css">
+  <link rel="stylesheet" href="css/success.css" type="text/css">
   <script src="js/jquery.min.js"></script>
 </head>
 <body>
+<div class="result">
+		<p id="head">สมัครเสร็จสมบูรณ์</p>
     <?php echo $_GET['result']; ?>
+    <br/>
+	    <a href="index.php">home</a></div>
+	</div>
 </body>
 </html>
--- regis_stu_success.php
+++ regis_stu_success.php
@@ -3,9 +3,15 @@
 <head>
     <title>Registation result</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+  	<link rel="stylesheet" href="css/success.css" type="text/css">
 </head>
 <body>
-    <?php echo $_GET['result']; ?>
-
+	<div class="result">
+		<p id="head">สมัครเสร็จสมบูรณ์</p>
+	    <?php echo $_GET['result']; ?>
+	    <br/>
+	    <a href="index.php">home</a></div>
+	</div>
 </body>
 </html>
