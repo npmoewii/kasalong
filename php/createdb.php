c01991d4807c58d82da4b8399e867d97348939a3
npmoewii <natchaporn.pon@gmail.com>
2016-10-23 16:52:23 +0700

    PHP Create database

---
+++ php/createdb.php
@@ -0,0 +1,93 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "12345678";
+    $dbname = "kasalong";
+
+    $conn = new mysqli($servername,$username,$password);
+
+    if($conn->connect_error){
+        echo "Connection Failed: ".$conn->connect_error;
+    }
+    else{
+        $sql = "CREATE DATABASE IF NOT EXISTS ".$dbname." CHARACTER SET utf8 COLLATE utf8_general_ci;";
+        if($conn->query($sql)){
+            $conn = new mysqli($servername,$username,$password,$dbname);
+
+            $str = array("CREATE TABLE IF NOT EXISTS info_student
+(
+    title text not null,
+    sname text not null,
+    ssurname text not null,
+    nickname text not null,
+    birth date not null,
+    nationid varchar(15) not null primary key,
+    religion text not null,
+    address text not null,
+    phone text not null,
+    email text not null,
+    facebook text not null,
+    clas int not null,
+    school text not null,
+    program text,
+    disease text,
+    size varchar(10) not null,
+    img_prof text not null,
+    pname text not null,
+    pphone text not null,
+    fname text,
+    fphone text,
+    mname text,
+    mphone text,
+    went boolean,
+    gpax float,
+    gpa float,
+    job text,
+    ent text,
+    future text,
+    ans1 text,
+    pass boolean
+ )
+","
+CREATE TABLE IF NOT EXISTS slip
+(
+    nationid varchar(15) not null primary key,
+    img_slip text,
+    recheckslip boolean null
+)
+","
+CREATE TABLE IF NOT EXISTS info_staff
+(
+    idstu varchar(10) not null primary key,
+    staffname text not null,
+    staffsurname text not null,
+    nickname text not null,
+    faculty text,
+    years int,
+    img_prof text not null,
+    livechr boolean not null,
+    goself boolean not null,
+    paid int,
+    size varchar(10) not null,
+    phone text not null,
+    lineid text null,
+    facebook text null
+)");
+            foreach($str as $sql){
+                if(!$conn->query($sql)){
+                    echo "SQL: ".$sql."<br>";
+                    echo "Create table error: ".$conn->connect_error."<br>";
+                }
+                else{
+                    echo "Created Success: ".$sql."<br>";
+                }
+            }
+
+
+        }
+        else{
+            echo "Create database error: ".$conn->connect_error;
+        }
+    }
+
+?>
