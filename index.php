8d5b36d796baa8621e8e838cd5d708aebc99b745
asddsada <baipo_p@hotmail.com>
2016-10-24 23:15:34 +0700

    login link edit + li index edit

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <li class="page-scroll"><a href="#section3">กำหนดการ</a></li>
         <li class="page-scroll"><a href="#section4">ถาม-ตอบ</a></li>
         <li class="page-scroll"><a href="#section5">ติดต่อสอบถาม</a></li>
-        <li class="right"><a href="upslip_1.php">Login</a></li>
+        <li class="right"><a href="slippage.php">Login</a></li>
       </ul>
     </div>
   </nav>
@@ -135,7 +135,7 @@
           พี่นัท 0584894484<br>
           พี่ตั้ม 0584894484<br>
           <br>
-          <h3>อัีเมลล์</h3>
+          <h3>อีเมลล์</h3>
           dfdsggs@fgsg.com
         </div>
       </ul>
