18a5381e9a5e6b207f9df355e5381de8f8533e25
doliolarzz <doliolarzz@hotmail.com>
2016-10-24 18:32:45 +0700

    แก้หลายๆอย่าง

--- slippage.php
+++ slippage.php
@@ -16,12 +16,12 @@
 			success: function(response) {
 				//alert(response);
 				if(response==true){
-					document.getElementById("indicator").innerHTML = "<img src='valid.png'>";
+					document.getElementById("indicator").innerHTML = "<img src='img/valid.png'>";
 					$("#ups").fadeIn(1000);
 					$("#sub").fadeIn(1000);
 				}
 				else{
-					document.getElementById("indicator").innerHTML = "<img src='invalid.png'>";
+					document.getElementById("indicator").innerHTML = "<img src='img/invalid.png'>";
 					$("#ups").fadeOut(1000);
 					$("#sub").fadeOut(1000);
 				}
@@ -32,15 +32,21 @@
 <head>
     <title>ส่งสลิป</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/slip.css" type="text/css">
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
-	<form action="php/upslip.php" method="post" enctype="multipart/form-data" onkeypress="return event.keyCode != 13;">
-        <input type="text" id="nationid" onkeyup="myFunction()" name="nationid" required>
-        <div id="indicator"></div>
-        <input id="ups" type="file" name="slipsub" accept="*.*" required>
-        <button id="sub" type="submit" name="btn-submit">Submit</button>
-     </form>
+	<div id="container">
+    <div id="conte">
+    <div id="hea">ใส่รหัสบัตรประชาชนเพื่อยืนยันสลิป</div>
+        <form id="ff" action="php/upslip.php" method="post" enctype="multipart/form-data" onkeypress="return event.keyCode != 13;">
+            <input type="text" id="nationid" onkeyup="myFunction()" autocomplete="off" name="nationid" required>
+            <div id="indicator"></div>
+            <input id="ups" type="file" name="slipsub" accept="*.*" required>
+            <button id="sub" type="submit" name="btn-submit">Submit</button>
+         </form>
+     </div>
+     </div>
 </body>
 </html>
