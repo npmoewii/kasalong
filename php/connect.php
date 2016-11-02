a8f9dedd57818e9d59461f63f8666e4563cd849c
blead <blead010@gmail.com>
2016-11-03 01:33:37 +0700

    modify connect.php and createdb.php to use variables from config.php

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,5 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "kasalong";
+    include("config.php");
 
     $conn = new mysqli($servername,$username,$password,$dbname);
     if($conn->connect_error){
